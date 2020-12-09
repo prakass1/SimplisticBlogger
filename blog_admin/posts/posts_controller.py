d36b9bac2e1de9fba0a283fabd3d40f01b6fca8c
Subash Prakash <subashprakash8@gmail.com>
2020-12-09 22:21:20 +0100

    updated html routes

--- blog_admin/posts/posts_controller.py
+++ blog_admin/posts/posts_controller.py
@@ -3,17 +3,17 @@ from flask_login import login_required, current_user
 
 posts_bp = Blueprint("posts", __name__)
 
-@posts_bp.route("/overview")
+@posts_bp.route("/overview.html")
 @login_required
 def dash_overview():
     return render_template("dashboard/overview.html", user=current_user)
 
-@posts_bp.route("/posts")
+@posts_bp.route("/posts.html")
 @login_required
 def dash_posts():
     return render_template("dashboard/posts.html", user=current_user)
 
-@posts_bp.route("/add_post")
+@posts_bp.route("/add_post.html")
 @login_required
 def add_post():
     return render_template("dashboard/add_post.html", user=current_user)
